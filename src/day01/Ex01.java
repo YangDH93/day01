cef6b84a3245e4455f9d8332fe4601c3d8a778e8
YD <fkrtn123@naver.com>
2022-03-16 11:33:01 +0900

    Ex01 print

---
+++ src/day01/Ex01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		
+/*		문
+		단
+		주
+		석        */
+		
+		System.out.println("\"내용\" : 문자열 표현에 사용");
+		System.out.println("java를 배우는 시간입니다.");
+		System.out.print("출력후 ");
+		System.out.print("줄바꿈이 읎다.");
+				
+	}
+}
